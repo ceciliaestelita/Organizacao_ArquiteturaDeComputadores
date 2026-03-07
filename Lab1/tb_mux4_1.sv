35ee9a90ff64c4e83cd5a55a44d1920a8fa7bd0e
Cecília Estelita <106453269+ceciliaestelita@users.noreply.github.com>
2026-03-07 11:10:17 -0300

    Update tb_mux4_1.sv

---
+++ Lab1/tb_mux4_1.sv
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+
+module tb_mux4x1;
+  logic [5:0] count;
+  logic muxOut;
+
+  mux4x1 dut(
+    .f   (muxOut),
+    .a   (count[5]),
+    .b   (count[4]),
+    .c   (count[3]),
+    .d   (count[2]),
+    .sel (count[1:0])
+  );
+
+  initial begin
+    $monitor($time, " a=%b | b=%b | c=%b | d=%b | sel=%b | out=%b",
+             count[5], count[4], count[3], count[2], count[1:0], muxOut);
+
+    for(count = 0; count <= 6'b111111; count++) #10;
+
+    #10 $finish;
+  end
+
+endmodule: tb_mux4x1
