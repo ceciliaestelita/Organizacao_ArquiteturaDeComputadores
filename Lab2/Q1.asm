5eb649f21ae3afe2a9545217645ce24dd42785e2
LucianoJRS <144392234+lucianojrs1@users.noreply.github.com>
2026-03-23 19:58:50 -0300

    Fix addition operation in Q1.asm

--- Lab2/Q1.asm
+++ Lab2/Q1.asm
@@ -2,4 +2,14 @@ lw t0, a
 lw t1, b           
 addi t2,x0, 0     
 bge t1, t2, condicao 
-add t2, t0, t1 
+add t2, t0, t1     
+
+condicao:
+sw t2, m           
+halt               
+
+
+a: .word 6
+b: .word 5
+m: .word 0
+
